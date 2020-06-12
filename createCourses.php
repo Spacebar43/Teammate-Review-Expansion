11c6d781fed7cbd0de312dc4269cbf2790d94590
ThatSourcerer <Slgreco@buffalo.edu>
2020-06-12 17:56:25 -0400

    Fixed bug where form could not insert in database #80

--- createCourses.php
+++ createCourses.php
@@ -55,13 +55,7 @@ hr {
 <hr>
 <?php
 
-  function addCourse ($connection, $courseNum, $courseName) {
-    echo "Added to database";
-    $sql = $connection->prepare( 'INSERT INTO course (code,name) values(?,?)' );
-    $sql->bind_param('is', $courseNum, $courseName);
-    $sql->execute();
-  }
-
+  require "lib/constants.php";
 
   //error logging
   error_reporting(-1); // reports all errors
@@ -70,56 +64,27 @@ hr {
   ini_set("error_log", "~/php-error.log");
 
   session_start();
-
-  require "lib/database.php";
-  require "lib/constants.php";
-
+  
   if(!isset($_SESSION['faculty_id'])) {
     header("Location: ".SITE_HOME."index.php");
     exit();
   }
 
+  require "lib/database.php";
   $con = connectToDatabase();
 
-
-
-  if(isset($_POST['facultyPasswordEntryText']) && !empty($_POST['facultyPasswordEntryText'])){
-
-    $code = $_POST['facultyPasswordEntryText'];
-    $stmt= $con->prepare('SELECT * FROM faculty_login WHERE password=?');
-    $stmt->bind_param('s',$code);
-    $stmt->execute();
-    $stmt->store_result();
-    if($stmt->num_rows == 0){
-          echo '<script language="javascript">';
-          echo 'alert("Code not found! Please check that you have typed the code correctly, or get a new one.")';
-          echo '</script>';
-          $stmt->close();
-          exit();
-    }
-
-    $time = time();
-
-    $stmt = $con->prepare('SELECT id, email FROM faculty_login WHERE password=? AND expiration_time > ?');
-    $stmt->bind_param('si',$code,$time);
-    $stmt->execute();
-    $stmt->store_result();
-    if($stmt->num_rows == 0){
-          echo '<script language="javascript">';
-          echo 'alert("Your access code has expired, please get a new code.")';
-          echo '</script>';
-    	    $stmt->close();
-    	    exit();
-    }
-    $stmt->bind_result($id,$email);
-    $stmt->fetch();
-
-    $courseNum = $_POST['courseNumberEntryText'];
-    $courseName = $_POST['courseNameEntryText'];
-
-    addCourse($con, $courseNum, $courseName);
-
+  function addCourse ($connection, $courseNum, $courseName) {
+    echo "Added to database";
+    $sql = $connection->prepare( 'INSERT INTO course (code,name) values(?,?)' );
+    $sql->bind_param('is', $courseNum, $courseName);
+    $sql->execute();
   }
+
+  $courseNum = $_POST['courseNumberEntryText'];
+  $courseName = $_POST['courseNameEntryText'];
+  echo "adding to database!";
+  addCourse($con, $courseNum, $courseName);
+
 ?>
 <hr>
 
