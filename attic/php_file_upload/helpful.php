1c85664b249e828690dbd42531edb3c9a8de4d0d
Phoenix Cerny <trcerny@cheshire.cse.buffalo.edu>
2020-06-16 23:30:00 -0400

    more work on research task #54 eventually for story #51

--- attic/php_file_upload/helpful.php
+++ attic/php_file_upload/helpful.php
@@ -8,3 +8,22 @@ if (in_array('purple', $acceptanceSet)){
 } else {
     print_r('it\'s not in the set :(');
 }
+
+// can make a redirect add to the end of the url
+if (false) {
+    header("Location: index.php?message_for_user");
+}
+
+/* the following code wasn't needed for script.php
+    // assign a unique id to the file and move it to the site TODO
+    // this isn't working but isn't needed
+    $fileNameNew = uniqid('', true).".".$fileExt;
+    $fileDestination = '/web/CSE442-542/2020-Summer/cse-442a/phoenix_test/attic/php_file_upload/'.$fileNameNew;//TODO
+    echo $fileNameNew.'<br>'.$fileDestination.'<br>';
+    // move the file from temporary location to new location
+    if (move_uploaded_file($fileTmpName, $fileDestination)) {
+        echo 'file moved successfully<br>';
+    } else {
+        echo 'file not moved successfully<br>';
+    } */
+
