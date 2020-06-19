df520398089707835fba2eae85490403da669301
ThatSourcerer <Slgreco@buffalo.edu>
2020-06-19 18:43:39 -0400

    Fixed typo and Faculty Login Text #96

--- facultyAccessCodePage.php
+++ facultyAccessCodePage.php
@@ -22,11 +22,11 @@ hr {
 <hr>
 <div id="login" class="w3-row-padding w3-center w3-padding">
   <form id="loginEmail" class="w3-container w3-card-4 w3-light-blue" method='post'>
-    <h2>To access your evaluation form, please enter the code here:</h2>
+    <h2>To access the Faculty Control Panel, please enter the code here:</h2>
     <div id="codeEntry" class="w3-section w3-center">
       <input placeholder="Enter access code here" name ='accessCodeEntryText' id="accessCodeEntryText" class="w3-input w3-light-grey" type="text" pattern="^[a-zA-Z0-9]*$" required>
       <hr>
-      <input type='submit' id="accessCodeEntryButton" class="w3-center w3-button w3-theme-dark" value='Acess Faculty Control Panel'></input>
+      <input type='submit' id="accessCodeEntryButton" class="w3-center w3-button w3-theme-dark" value='Access Faculty Control Panel'></input>
       <h2>If your code is older than 15 minutes, click here to get a new one.</h2>
       <input type='button' onclick="window.location.href = 'index.php';" class="w3-center w3-button w3-theme-dark" value="Dont have a valid code?"/></input>
       <hr>
