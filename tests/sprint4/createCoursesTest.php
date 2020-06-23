d9368f2c74f812d6cf392103d3da1a2b17495b0b
ThatSourcerer <Slgreco@buffalo.edu>
2020-06-23 10:20:17 -0400

    Added redirect in tests dir for testing Tasks/Sories #52

---
+++ tests/sprint4/createCoursesTest.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+// Test Create Courses Functionality Test
+
+//Redirects to directly to createCourses.php
+
+// Set the sesion data
+session_start();
+session_regenerate_id();
+$_SESSION['loggedin'] = TRUE;
+$_SESSION['email'] = 'slgreco@buffalo.edu';
+$_SESSION['id'] = '2';
+$_SESSION['faculty_id'] = '2';
+header("Location: ../../CreateCourses.php");
+
+?>
