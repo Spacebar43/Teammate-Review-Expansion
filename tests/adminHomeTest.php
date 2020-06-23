feeeb6f999b70fe9ff04b199a74c2ddc4f033f42
ThatSourcerer <Slgreco@buffalo.edu>
2020-06-23 10:59:24 -0400

    Addded redirect to reach adminHome page for testing Task/Stories #54 #2

---
+++ tests/adminHomeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+// Test Getting Raw Evals CSV Functionality Test
+
+//Redirects to directly to getEvals.php
+
+// Set the sesion data
+session_start();
+session_regenerate_id();
+$_SESSION['loggedin'] = TRUE;
+$_SESSION['email'] = 'slgreco@buffalo.edu';
+$_SESSION['id'] = '2';
+$_SESSION['faculty_id'] = '2';
+header("Location: ../../adminHome.php");
+
+?>
